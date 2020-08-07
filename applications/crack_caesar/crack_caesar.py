465f02a2a9bafbd03ba6bd1c377104a0f434bf5d
Elysia Gabe <elysiagabe@gmail.com>
2020-08-06 23:44:52 -0700

    Completes more than 5 of 8 challenges (7 total)

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -11,22 +11,18 @@ with open("ciphertext.txt") as f:
     text = f.read()
 
 # only find counts of letters in the alphabet
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 for char in text:
     if char not in letter_count and char.isalpha():
         letter_count[char] = 1
     elif char in letter_count and char.isalpha():
         letter_count[char] += 1
 
+# sort by frequency
 sorted_count = dict(sorted(letter_count.items(), key=lambda x: x[1], reverse=True))
 
-print(sorted_count)
-
+# from read me...in order of frequency
 sorted_alph_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
-
-# modified = dict(map(lambda x, y: x * y, sorted_count, [2] * len(sorted_alph_list)))
 
 # replace the numeric value with the letter by frequency (sorted_alph_list from README)
 modifiedList = dict(map(lambda x, y: x + y, sorted_count, sorted_alph_list))
